0afcc8273c68f162a42403b6c046312135257877
Devang <acw.dnsp@gmail.com>
2020-11-28 12:15:44 +0000

    commit-1

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 1111;
+const path = require("path");
 const routes = require('./routes/api');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config();
-
+app.use(express.static("client/build"));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 //connect to DB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("Db connected!"));
 
@@ -15,6 +19,10 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/dataItem', routes);
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
