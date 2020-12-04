97f958c9b1d345f72ba703f2f1df73fe488906ed
Devang <acw.dnsp@gmail.com>
2020-12-04 15:42:57 +0000

    dsfds

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ dotenv.config();
 
 //running react when server is starting
 app.use(express.static("client/build"));
-// app.get('*', (req,res) =>{
-//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("Db connected!"));
