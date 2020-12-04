bb4f2c4ee58a78db843d4db1b5b689e4c4d4bb58
Devang <acw.dnsp@gmail.com>
2020-12-04 18:08:12 +0000

    sdfdsf

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ app.use(express.json());
 app.use(cors());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
+
 app.get('/', (req, res) => {
   Data.find({}).then(data => res.json(data));
 })
@@ -50,6 +51,13 @@ try{
 }
 })
 
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
