a853d40a0c7c8a7af7b67d2ba95890ae3c8a7aef
Devang <acw.dnsp@gmail.com>
2020-12-04 18:34:56 +0000

    sdfdsf

--- app.js
+++ app.js
@@ -52,6 +52,9 @@ try{
 })
 
 app.use(express.static('client/build'));
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 // app.use(express.static(path.join(__dirname, "client/build")));
 // app.use(express.static("public"));
 
