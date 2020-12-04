78aa94c5d9192131ab5d178f39e428975b9c5585
Devang <acw.dnsp@gmail.com>
2020-12-04 18:12:24 +0000

    sdfdsf

--- app.js
+++ app.js
@@ -51,13 +51,13 @@ try{
 }
 })
 
+app.use(express.static(__dirname, 'client/build'));
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.use(express.static("public"));
-
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
