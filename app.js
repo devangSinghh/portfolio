a29af76ccf4d6eacc14d36c9af2447350c0e78cf
Devang <acw.dnsp@gmail.com>
2020-12-04 17:53:38 +0000

    sdfdsf

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/client/build')));
-
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '/my-app/build/index.html'));
+});
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
